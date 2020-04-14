4932f292c8ce936f4a5b79215b0ab95bec0bbb41
fork_web <leshadgrad@mail.ru>
2020-04-15 00:50:37 +0400

    start-header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App!!!"/>
+
+
+
+    <section class="header">
+      
+      <div class="container">
+
+        <div class="head-title">
+          <h1>Clear, smart, attractive design</h1>
+          <h2>for your app, logo, website</h2>
+        </div>
+
+      </div>
+
+    </section>
+
+
+  <template> 
+    <Portfolio/>
+  </template>
+
+  
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Portfolio from './components/Portfolio.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Portfolio
   }
 }
 </script>
---
+++ src/components/Portfolio.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <div class="container">
+      <h1>PORTFOLIO</h1>
+    </div>
+  </section>  
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+}
+</script>
+
+<style lang="scss" type="text/scss">
+  @import "../style/main.scss";
+  @import "../style/portfolio.scss";
+</style>
